8a5da3ce0e9ccbf694d66ded4321ae396a313b3d
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-12-10 05:52:54 +0800

    Initial commit

---
+++ POS System Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace POS_System_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
